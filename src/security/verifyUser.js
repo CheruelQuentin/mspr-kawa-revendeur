c0f52688754ca937a0c1ce6aa41f7791fa7f360f
Lucas Belin <lucasbelinpro@gmail.com>
2023-06-20 14:41:10 +0200

    fix: request authentication

--- src/security/verifyUser.js
+++ src/security/verifyUser.js
@@ -1,21 +1,22 @@
-'use strict';
+"use strict"
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken")
 
 const validateUserIdToken = async (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.headers["authorization"]
+  const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.headers["authorization"]
+  console.log(token)
 
-    if (!token) {
-        return res.status(403).send("A token is required for authentication")
-    }
-    try {
-        req.user = jwt.verify(token, process.env.TOKEN_KEY)
-    } catch (err) {
-        return res.status(401).send("Invalid Token")
-    }
-    return next()
-};
+  if (!token) {
+    return res.status(403).send("A token is required for authentication")
+  }
+  try {
+    req.user = jwt.verify(token.split(" ")[1], process.env.TOKEN_KEY)
+  } catch (err) {
+    return res.status(401).send("Invalid Token")
+  }
+  return next()
+}
 
 module.exports = {
-    validateFirebaseIdToken: validateUserIdToken
-};
+  validateFirebaseIdToken: validateUserIdToken,
+}
